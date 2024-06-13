e43d91ffd1e9a1843f8ba11dacb0ce874973aa44
Sattawat Wongsuddee <sattawat.w@7solutions.co.th>
2024-06-14 01:37:42 +0700

    add dropdown option card

---
+++ src/app/dropdown/option-card.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+interface OptionProps extends React.ButtonHTMLAttributes<HTMLOptionElement> {
+
+}
+
+export function OptionCard(
+    {
+        optionProps,
+        children
+    }: {
+        optionProps: OptionProps,
+        children: string
+    }
+) {
+    return (
+        <option
+            className="text-center text-base text-black	"
+            value={optionProps.value}
+            data-testid={`option-${optionProps.value}`}
+            onClick={() => { console.log(optionProps.value) }}
+        >
+            {children}
+        </option>
+    )
+}
