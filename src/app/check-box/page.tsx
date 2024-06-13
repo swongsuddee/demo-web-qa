d6c0fcac7689e97f79301940cac05cbdfe087284
Sattawat Wongsuddee <sattawat.w@7solutions.co.th>
2024-06-14 02:14:19 +0700

    add radio and check box page

--- src/app/check-box/page.tsx
+++ src/app/check-box/page.tsx
@@ -1,38 +1,37 @@
-// import { Metadata } from "next";
-// import { BoxCard } from "../../components/box-card";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//     title: '4. Check box',
-// };
+export const metadata: Metadata = {
+    title: '4. Check box',
+};
 
-// const CheckBox = (() => {
-//     const colors = [
-//         { name: 'Red', disabled: false, checked: false },
-//         { name: 'Orange', disabled: false, checked: true },
-//         { name: 'Yellow', disabled: false, checked: false },
-//         { name: 'Green', disabled: false, checked: false },
-//         { name: 'Blue', disabled: true, checked: false },
-//         { name: 'Navy', disabled: true, checked: true },
-//         { name: 'Purple', disabled: false, checked: false }
-//     ]
+const CheckBox = (() => {
+    const colors = [
+        { name: 'Red', disabled: false, checked: false },
+        { name: 'Orange', disabled: false, checked: true },
+        { name: 'Yellow', disabled: false, checked: false },
+        { name: 'Green', disabled: false, checked: false },
+        { name: 'Blue', disabled: true, checked: false },
+        { name: 'Navy', disabled: true, checked: true },
+        { name: 'Purple', disabled: false, checked: false }
+    ]
 
-//     return (
-//         <div>
-//             <div className="py-4">
-//                 What is yout favorits colors?
-//             </div>
-//             {colors.map(item =>
-//                 <BoxCard
-//                     cardName={item.name}
-//                     inputProps={{
-//                         type: "checkbox",
-//                         checked: item.checked,
-//                         disabled: item.disabled
-//                     }}
-//                     datatestid={item.name}
-//                 />)}
-//         </div>
-//     )
-// });
+    return (
+        <div>
+            {/* <div className="py-4">
+                What is yout favorits colors?
+            </div>
+            {colors.map(item =>
+                <BoxCard
+                    cardName={item.name}
+                    inputProps={{
+                        type: "checkbox",
+                        checked: item.checked,
+                        disabled: item.disabled
+                    }}
+                    datatestid={item.name}
+                />)} */}
+        </div>
+    )
+});
 
-// export default CheckBox;
+export default CheckBox;
