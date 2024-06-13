8f047047b111a6f3efd973bcafaf9e07dab99d7b
Sattawat Wongsuddee <sattawat.w@7solutions.co.th>
2024-06-14 01:51:42 +0700

    comment all concerned with card box

--- src/app/radio-box/page.tsx
+++ src/app/radio-box/page.tsx
@@ -1,78 +1,78 @@
-import { Metadata } from "next";
-import { BoxCard } from "../../components/box-card";
+// import { Metadata } from "next";
+// import { BoxCard } from "../../components/box-card";
 
-export const metadata: Metadata = {
-    title: '5. Radio box',
-};
+// export const metadata: Metadata = {
+//     title: '5. Radio box',
+// };
 
-const CheckBox = (() => {
-    const selectedDrink = "Coffee";
-    const drinks = [
-        { name: 'Coffee', disabled: false, checked: false },
-        { name: 'Tea', disabled: false, checked: false },
-        { name: 'Cola', disabled: true, checked: false }
-    ];
+// const CheckBox = (() => {
+//     const selectedDrink = "Coffee";
+//     const drinks = [
+//         { name: 'Coffee', disabled: false, checked: false },
+//         { name: 'Tea', disabled: false, checked: false },
+//         { name: 'Cola', disabled: true, checked: false }
+//     ];
 
-    const selectedFreq = "";
-    const freq = [
-        { name: 'Usually', disabled: false, checked: false },
-        { name: 'Often', disabled: false, checked: false },
-        { name: 'Sometimes', disabled: false, checked: true },
-        { name: 'Rarely', disabled: false, checked: false },
-        { name: 'Never', disabled: false, checked: false }
-    ]
+//     const selectedFreq = "";
+//     const freq = [
+//         { name: 'Usually', disabled: false, checked: false },
+//         { name: 'Often', disabled: false, checked: false },
+//         { name: 'Sometimes', disabled: false, checked: true },
+//         { name: 'Rarely', disabled: false, checked: false },
+//         { name: 'Never', disabled: false, checked: false }
+//     ]
 
-    return (
-        <div>
-            <div>
-                <div className="py-4">
-                    Which one is your prefered drink?
-                </div>
-                <form>
-                    <fieldset>
-                        {
-                            drinks.map(item =>
-                                <label key={item.name} className="flex items-center space-x-2" >
-                                    <BoxCard
-                                        cardName={item.name}
-                                        inputProps={{
-                                            type: "radio",
-                                            checked: item.name === selectedDrink,
-                                            disabled: item.disabled
-                                        }}
-                                        datatestid={item.name}
-                                    />
-                                </label>
-                            )
-                        }
-                    </fieldset>
-                </form>
-            </div>
-            <div>
-                <div className="py-4">
-                    How often do you drink {selectedDrink}?
-                </div>
-                <form>
-                    <fieldset>
-                        {
-                            freq.map(item =>
-                                <BoxCard
-                                    cardName={item.name}
-                                    inputProps={{
-                                        type: "radio",
-                                        checked: item.name === selectedFreq,
-                                        disabled: item.disabled
-                                    }}
-                                    datatestid={item.name}
-                                />
-                            )
-                        }
-                    </fieldset>
-                </form>
-            </div>
+//     return (
+//         <div>
+//             <div>
+//                 <div className="py-4">
+//                     Which one is your prefered drink?
+//                 </div>
+//                 <form>
+//                     <fieldset>
+//                         {
+//                             drinks.map(item =>
+//                                 <label key={item.name} className="flex items-center space-x-2" >
+//                                     <BoxCard
+//                                         cardName={item.name}
+//                                         inputProps={{
+//                                             type: "radio",
+//                                             checked: item.name === selectedDrink,
+//                                             disabled: item.disabled
+//                                         }}
+//                                         datatestid={item.name}
+//                                     />
+//                                 </label>
+//                             )
+//                         }
+//                     </fieldset>
+//                 </form>
+//             </div>
+//             <div>
+//                 <div className="py-4">
+//                     How often do you drink {selectedDrink}?
+//                 </div>
+//                 <form>
+//                     <fieldset>
+//                         {
+//                             freq.map(item =>
+//                                 <BoxCard
+//                                     cardName={item.name}
+//                                     inputProps={{
+//                                         type: "radio",
+//                                         checked: item.name === selectedFreq,
+//                                         disabled: item.disabled
+//                                     }}
+//                                     datatestid={item.name}
+//                                 />
+//                             )
+//                         }
+//                     </fieldset>
+//                 </form>
+//             </div>
 
-        </div >
-    )
-});
+//         </div >
+//     )
+// });
 
-export default CheckBox;
+// export default CheckBox;
--- src/components/box-card.tsx
+++ src/components/box-card.tsx
@@ -1,38 +1,38 @@
-'use client'
+// 'use client'
 
-import { useState } from "react";
+// import { useState } from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-}
+// interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+// }
 
-export function BoxCard(
-    {
-        cardName,
-        inputProps,
-        datatestid
-    }: {
-        cardName: string,
-        inputProps: InputProps,
-        datatestid: string
-    }
-) {
-    return (
-        <div className="py-4">
-            {/* <input
-                className="mr-2"
-                type={inputProps.type}
-                key={cardName}
-                value={cardName}
-                checked={inputProps.checked}
-                disabled={inputProps.disabled}
-                data-testid={datatestid}
+// export function BoxCard(
+//     {
+//         cardName,
+//         inputProps,
+//         datatestid
+//     }: {
+//         cardName: string,
+//         inputProps: InputProps,
+//         datatestid: string
+//     }
+// ) {
+//     return (
+//         <div className="py-4">
+//             {/* <input
+//                 className="mr-2"
+//                 type={inputProps.type}
+//                 key={cardName}
+//                 value={cardName}
+//                 checked={inputProps.checked}
+//                 disabled={inputProps.disabled}
+//                 data-testid={datatestid}
 
 
-                onClick={() => {
-                    console.log({ cardName })
-                }}
-            /> */}
-            <span>{cardName}</span>
-        </div>
-    );
-}
+//                 onClick={() => {
+//                     console.log({ cardName })
+//                 }}
+//             /> */}
+//             <span>{cardName}</span>
+//         </div>
+//     );
+// }
