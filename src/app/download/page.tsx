a1ae32681f5139295019dd7c72f269d4bba55c0c
Sattawat Wongsuddee <sattawat.w@7solutions.co.th>
2024-06-14 04:40:45 +0700

    remove router

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,39 +1,22 @@
 import { Metadata } from "next";
-import Image from 'next/image'
-import { useRouter } from "next/router";
-
 export const metadata: Metadata = {
     title: '7. Download',
 };
 
 const DownloadPage = (() => {
-    const router = useRouter();
-    const basePath = router.basePath;
 
     return (
-        <div className="justify-center">
-            <Image
+        <div>
+            {/* <Image
                 className="justify-self-center"
                 src="/image.webp"
                 width={200}
                 height={200}
                 alt="Dummy image for download"
+                priority={false}
             />
             <br />
-            <a href={`${basePath}/image.png`} download="image.webp">
-                <button
-                    className="
-                    justify-self-center
-                    bg-amber-500 
-                    text-white 
-                    px-4 
-                    py-2 
-                    rounded 
-                    hover:bg-amber-400 
-                    ">
-                    Download Image
-                </button>
-            </a>
+            <DownloadHandler /> */}
         </div>
     )
 });
